e57d4af9f25000923387dc4e508ee0006dc9c786
Gharib2020678 <69073633+Gharib2020678@users.noreply.github.com>
2021-02-08 23:52:56 +0300

    up

---
+++ DeleteFunction.php
@@ -0,0 +1,11 @@
+<?php 
+include_once("inc/config.php");
+$ID=$_GET['ID'];
+$page=$_GET['page'];
+if($page=="Familes")
+{
+$del_cate = mysqli_query ($conn, "DELETE FROM family WHERE Family_ID = '$ID'");
+}
+
+
+ ?>
---
+++ inc/config.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$BD_name = "elbaskallonbox";
+
+$conn = mysqli_connect($host,$username,$password,$BD_name); 
+mysqli_set_charset ($conn, "utf8");
+
+if (!$conn){
+
+echo mysqli_connect_error ("خطا فى عملية الاتصال") . mysqli_connect_errorno();
+
+}
+
+/*
+function close_db(){
+global $coon;
+mysqli_close($coon);
+
+}*/
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+ ?>
